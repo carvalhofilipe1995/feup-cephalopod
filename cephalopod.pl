3e912a404380d0235f191d72bcc587e33de73154
Luís Carvalho <carvalhofilipe1995@gmail.com>
2016-10-08 02:36:52 +0100

    representation

--- cephalopod.pl
+++ cephalopod.pl
@@ -5,11 +5,11 @@
 %% Cephalopod is a two player game which employs a 5x5 board, 24 dice of one color, and 24 dice of another color.
 
 board([
-         [[],[],[],[],[]],
-         [[],[],[],[],[]],
-         [[],[],[],[],[]],
-         [[],[],[],[],[]],
-         [[],[],[],[],[]]
+        [1-2, 0-0, 0-0, 0-0, 0-0],
+        [1-2, 0-0, 0-0, 0-0, 0-0],
+        [1-2, 0-0, 0-0, 0-0, 0-0],
+        [1-2, 0-0, 0-0, 0-0, 0-0],
+        [1-2, 0-0, 0-0, 0-0, 0-0]
       ]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -20,12 +20,20 @@ board([
 %%%%%%%%%%%%% BOARD %%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% 1-X -> 1 Represents the white color and the X represents the value of the piece 
+% 2-X -> 2 Represents the black color and the X represents the value of the piece 
+% 0-0 -> Represents an empty cell 
+
 % Printing Board
 
+convertValues(0,0):- write('').                 %% Empty Cell
+convertValues(1,Number):- write(Number).        %% White Cell
+convertValues(2,Number):- write(Number).        %% Black Cell
+                       
 drawBoard([]).
 drawBoard([FirstLine | Rest]):- showLine(FirstLine), drawBoard(Rest).
 
 showLine([]).
-showLine([FirstCell | Rest]):- write(FirstCell), showLine(Rest).
+showLine([ColorDie - NumberDie | Rest]):- convertValues(ColorDie,NumberDie), showLine(Rest).
 
 
