5243c62875edefc807e9061bf9ec2df878b42c31
Luís Carvalho <carvalhofilipe1995@gmail.com>
2016-10-14 17:53:54 +0100

    board

--- cephalopod.pl
+++ cephalopod.pl
@@ -1,24 +1,22 @@
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%% CEPHALOPOD %%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Cephalopod is a two player game which employs a 5x5 board, 24 dice of one color, and 24 dice of another color.
 
-board([
-        [1-2, 0-0, 0-0, 0-0, 0-0],
-        [1-2, 0-0, 0-0, 0-0, 0-0],
-        [1-2, 0-0, 0-0, 0-0, 0-0],
-        [1-2, 0-0, 0-0, 0-0, 0-0],
-        [1-2, 0-0, 0-0, 0-0, 0-0]
-      ]).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%% LOGIC %%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%% BOARD %%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+board([
+        [1-2, 2-0, 0-0, 0-0, 0-0],
+        [1-2, 2-0, 0-0, 0-0, 0-0],
+        [1-2, 2-0, 0-0, 0-0, 0-0],
+        [1-2, 2-0, 0-0, 0-0, 0-0],
+        [1-2, 2-0, 0-0, 0-0, 0-0]
+      ]).
+
 
 % 1-X -> 1 Represents the white color and the X represents the value of the piece 
 % 2-X -> 2 Represents the black color and the X represents the value of the piece 
@@ -26,14 +24,30 @@ board([
 
 % Printing Board
 
-convertValues(0,0):- write('').                 %% Empty Cell
-convertValues(1,Number):- write(Number).        %% White Cell
-convertValues(2,Number):- write(Number).        %% Black Cell
+cellDivision:- write('|'). 
+columns:- write('          |    1    |    2    |    3    |    4    |    5    |').
+
+lineConvert(LineNumber):- write('|    '), write(LineNumber), write('    ').
+
+convertValues(0, 0):- write('         ').                                      %% Empty Cell
+convertValues(1, Number):- write(' WHITE-'), write(Number), write(' ').        %% White Cell
+convertValues(2, Number):- write(' BLACK-'), write(Number), write(' ').        %% Black Cell
                        
-drawBoard([]).
-drawBoard([FirstLine | Rest]):- showLine(FirstLine), drawBoard(Rest).
+drawBoard([], _).
+drawBoard([FirstLine | Rest], Counter):-  
+        lineConvert(Counter), 
+        showLine(FirstLine), 
+        nl, 
+        NewCounter is Counter + 1, 
+        drawBoard(Rest, NewCounter).
 
-showLine([]).
-showLine([ColorDie - NumberDie | Rest]):- convertValues(ColorDie,NumberDie), showLine(Rest).
+showLine([]):- write('|').
+showLine([ColorDie - NumberDie | Rest]):- 
+        cellDivision, 
+        convertValues(ColorDie,NumberDie), 
+        showLine(Rest).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%% LOGIC %%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
